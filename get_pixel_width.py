68225f9ca32c37173b90aedd6165ceaa7466296c
CoopTRUE <coopdog4545@gmail.com>
2021-06-02 15:49:14 -0700

    file for finding pixel widths. Not implimented yet though

--- get_pixel_width.py
+++ get_pixel_width.py
@@ -1,16 +1,23 @@
-chars = "  .:!+*e$@8"
+chars = "raul"
 length = len(chars)-1
-
-
+print(length)
+import sys
+sys.setrecursionlimit(999999999)
+global t
+t = []
 def closer(num, i):
     i += 1
-    if i>600: return
-    print(num, 255/num)
+    if len(t) > 10:
+        if t[-1] == t[-3]:
+            print(i)
+            return
+    t.append((num, 255/num))
     if 255/num >= length:
         closer(round(num+.01, 2), i)
     else:
         closer(round(num-.01, 2), i)
 
-closer(30, 0)
+closer(100, 0)
+print(t[-2:])
 # import string
 # print(string.printable)
